55937ee38b2c6e9e985965174999f07d93ccaffb
Ilia <lelikk2002@gmail.com>
2023-11-25 22:15:34 +0200

    Minor changes in db

--- gerahelper/init_gerahelperdb.sql
+++ gerahelper/init_gerahelperdb.sql
@@ -2,7 +2,7 @@ CREATE TABLE organisations
 (
     Id       SERIAL PRIMARY KEY,
     Country  CHARACTER VARYING(50),
-    Name     CHARACTER VARYING(50) NO NULL,
+    Name     CHARACTER VARYING(50) NOT NULL,
     Contacts CHARACTER VARYING(100),
     Type     CHARACTER VARYING(50)
 );
@@ -11,14 +11,14 @@ CREATE TABLE users
 (
     Id      SERIAL PRIMARY KEY,
     Name    CHARACTER VARYING(30),
-    Contact CHARACTER VARYING(30) UNIQUE NO NULL
+    Contact CHARACTER VARYING(30) UNIQUE NOT NULL
 );
 
 CREATE TABLE issues
 (
     Id              SERIAL PRIMARY KEY,
     Status          CHARACTER VARYING(30),
-    Description     CHARACTER VARYING(300),
+    Description     TEXT,
     Organisation_id INTEGER,
     Validation      BOOLEAN,
     CONSTRAINT fk_organisation FOREIGN KEY (Organisation_id) REFERENCES organisations (Id)
@@ -28,13 +28,13 @@ CREATE TABLE messages
 (
     Id       SERIAL PRIMARY KEY,
     Data     TIMESTAMP,
-    Date     CHARACTER VARYING(4000),
+    Date     TEXT,
     Issue_id INTEGER,
     CONSTRAINT fk_author FOREIGN KEY (Issue_id) REFERENCES issues (Id)
 );
 
 CREATE PROCEDURE insert_issue(UserName CHARACTER VARYING (50), UserContactInfo CHARACTER VARYING (100),
-                              Description CHARACTER VARYING (300), OrganisationName CHARACTER VARYING (50),
+                              Description TEXT, OrganisationName CHARACTER VARYING (50),
                               OrganisationCountry CHARACTER VARYING (50),
                               OrganisationContactInfo CHARACTER VARYING (100),
                               OrganisationType CHARACTER VARYING (50))
